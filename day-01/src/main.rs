ca439a01df2ae42f72bbba77934563e53b2ac05d
Alex Wass <alex@wass.sh>
2025-12-06 21:35:12 +0000

    ✨ Complete day 1 part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -3,8 +3,13 @@ use std::io::{self, BufRead};
 
 fn main() {
     let file = load_puzzle();
-
     let start: i32 = 50;
+
+    part_1(&file, start);
+    part_2(&file, start);
+}
+
+fn part_1(file: &Vec<String>, start: i32) {
     let mut position: i32 = start;
     let mut count: i32 = 0;
 
@@ -20,6 +25,38 @@ fn main() {
         if position == 0 {
             count += 1;
         }
+    }
+
+    println!("Count: {}", count);
+}
+
+fn part_2(file: &Vec<String>, start: i32) {
+    let mut position: i32 = start;
+    let mut count: i32 = 0;
+
+    for line in file {
+        let direction = match line.chars().next() {
+            Some('L') => -1,
+            _ => 1,
+        };
+        let steps = line[1..].parse::<i32>().unwrap();
+
+        let mut steps_until_zero: i32 = if direction == 1 {
+            (100 - position) % 100
+        } else {
+            position % 100
+        };
+
+        if steps_until_zero == 0 {
+            steps_until_zero = 100;
+        }
+
+        if steps_until_zero <= steps {
+            let full_rotations = (steps - steps_until_zero) / 100;
+            count += 1 + full_rotations;
+        }
+
+        position = ((position + (direction * steps)) % 100 + 100) % 100;
     }
 
     println!("Count: {}", count);
